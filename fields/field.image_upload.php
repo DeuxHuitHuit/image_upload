e855ae862cd15b42e456f8c2358a676f896c802b
ChoboHub <antoine_lucas_999@Hotmail.com>
2016-08-16 11:05:48 -0400

    refactor null

--- fields/field.image_upload.php
+++ fields/field.image_upload.php
@@ -189,7 +189,7 @@
 			$wrapper->appendChild($div);
 		}
 
-		public function buildValidationSelect(XMLElement &$wrapper, $selected = NULL, $name = 'fields[validator]', $type = 'input', array $errors = NULL){
+		public function buildValidationSelect(XMLElement &$wrapper, $selected = null, $name = 'fields[val...', $type = 'input', array $errors = null){
 
 			include(TOOLKIT.'/util.validators.php');
 
